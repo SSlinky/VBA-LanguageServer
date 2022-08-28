5f6dcf90375b376842b51dc8a05c83e73650faad
Sslinky <svanderslink@hotmail.com>
2022-08-29 01:41:53 +0800

    refactoring and bug fixes

--- server/src/capabilities/vbaFolding.ts
+++ server/src/capabilities/vbaFolding.ts
@@ -1,21 +1,21 @@
 import { TextDocument } from 'vscode-languageserver-textdocument';
-import { FoldingRange, integer } from 'vscode-languageserver';
+import { FoldingRange } from 'vscode-languageserver';
+import { getMatchIndices, indexOfNearestUnder } from '../utils/helpers';
 
-export function getFoldingRanges(doc: TextDocument, maxRanges: number): FoldingRange[] {
-	// const range = Range.create(Position.create(0, 0), Position.create(doc.lineCount, 0));
+export function getFoldingRanges(doc: TextDocument, maxRanges?: number): FoldingRange[] {
 	const results: FoldingRange[] = [];
 
 	const reOpenPattern = /^(?:\s*)(?:(?:Public|Private|Friend)\s+)?(?:(?:Static)\s+)?(Function|Sub|Property|Enum|If.*Then\s*(?![^'\n])|Select Case|With|Do|While)(?:[\s\n])/gmi;
 	const reClosePattern = /(?:\n\s*)(End|Wend|Loop)\b/g;
 	const text = doc.getText();
 
-	const starts = getMatchIndices(reOpenPattern, text);
-	const ends = getMatchIndices(reClosePattern, text);
+	const starts = getMatchIndices(reOpenPattern, text, maxRanges);
+	const ends = getMatchIndices(reClosePattern, text, maxRanges);
 	ends.reverse();
 
 	while (starts.length > 0 && ends.length > 0) {
 		const pairedEnd = ends.pop()!;
-		const startIdx = priceIsRight(starts, pairedEnd);
+		const startIdx = indexOfNearestUnder(starts, pairedEnd);
 		if (startIdx === -1) { return results; }
 		const pairedStart = starts[startIdx];
 		starts.splice(startIdx, 1);
@@ -28,27 +28,4 @@ export function getFoldingRanges(doc: TextDocument, maxRanges: number): FoldingR
 	}
 
 	return results;
-}
-
-function getMatchIndices(re: RegExp, text: string): integer[] {
-	let m = re.exec("");
-	const results: integer[] = [];
-	while ((m = re.exec(text))) { results.push(groupIndex(m, 1)); }
-	return results;
-}
-
-// Returns the index of the nearest-but-not-under price number in sortedNums.
-function priceIsRight(sortedNums: integer[], price: integer): integer {
-	if (sortedNums.length === 0) { return -1; }
-	for (let i = 0; i < sortedNums.length; i++) {
-		const result = price - sortedNums[i];
-		if (result === 0) { return i; }
-		if (result < 0) { return i -1; }
-	}
-	return sortedNums.length;
-}
-
-// Returns the index of a group rather than the whole match.
-function groupIndex(reExec: RegExpExecArray, i: integer) {
-	return reExec.index + reExec[0].indexOf(reExec[i]);
 }
