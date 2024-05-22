aea85a5195fa37c93042b35765a2dbfc63c3ae97
sslinky <sam.vanderslink@notis.net.au>
2024-05-22 11:21:41 +0800

    Create parser

---
+++ server/src/project/elements/memory.ts
@@ -0,0 +1,189 @@
+import { AmbiguousIdentifierContext, AsTypeClauseContext, ConstStmtContext, ConstSubStmtContext, EnumerationStmtContext, EnumerationStmt_ConstantContext, MethodStmtContext, VariableStmtContext, VariableSubStmtContext } from '../../antlr/out/vbaParser';
+
+import { TextDocument } from 'vscode-languageserver-textdocument';
+
+import { BaseContextSyntaxElement, BaseSyntaxElement, HasSemanticToken, HasSymbolInformation } from './base';
+import { SemanticTokenModifiers, SemanticTokenTypes, SymbolInformation, SymbolKind } from 'vscode-languageserver';
+import { FoldableElement } from './special';
+import { SymbolInformationFactory } from '../../capabilities/symbolInformation';
+import '../../extensions/parserExtensions';
+
+
+export class IdentifierElement extends BaseContextSyntaxElement {
+	constructor(ctx: AmbiguousIdentifierContext, doc: TextDocument) {
+		super(ctx, doc);
+	}
+}
+
+
+abstract class BaseEnumElement extends FoldableElement implements HasSemanticToken, HasSymbolInformation {
+	identifier: IdentifierElement;
+	tokenModifiers: SemanticTokenModifiers[] = [];
+	abstract tokenType: SemanticTokenTypes;
+	abstract symbolKind: SymbolKind;
+
+	constructor(context: EnumerationStmtContext | EnumerationStmt_ConstantContext, document: TextDocument) {
+		super(context, document);
+		this.identifier = new IdentifierElement(context.ambiguousIdentifier(), document);
+	}
+
+	get name(): string { return this.identifier.text; }
+	get symbolInformation(): SymbolInformation {
+		return SymbolInformationFactory.create(
+			this, this.symbolKind
+		);
+	}
+
+}
+
+
+export class EnumBlockDeclarationElement extends BaseEnumElement {
+	tokenType: SemanticTokenTypes;
+	tokenModifiers: SemanticTokenModifiers[] = [];
+	symbolKind: SymbolKind;
+
+	constructor(context: EnumerationStmtContext, document: TextDocument) {
+		super(context, document);
+		this.tokenType = SemanticTokenTypes.enum;
+		this.symbolKind = SymbolKind.Enum;
+	}
+}
+
+
+export class EnumMemberDeclarationElement extends BaseEnumElement {
+	tokenType: SemanticTokenTypes;
+	tokenModifiers: SemanticTokenModifiers[] = [];
+	symbolKind: SymbolKind;
+
+	constructor(context: EnumerationStmt_ConstantContext, document: TextDocument) {
+		super(context, document);
+		this.tokenType = SemanticTokenTypes.enumMember;
+		this.symbolKind = SymbolKind.EnumMember;
+	}
+}
+
+abstract class BaseMethodElement extends FoldableElement implements HasSemanticToken, HasSymbolInformation {
+	identifier: IdentifierElement;
+	tokenModifiers: SemanticTokenModifiers[] = [];
+	abstract tokenType: SemanticTokenTypes;
+	abstract symbolKind: SymbolKind;
+
+	constructor(context: MethodStmtContext, document: TextDocument) {
+		super(context, document);
+		this.identifier = new IdentifierElement(context.methodSignatureStmt().ambiguousIdentifier(), document);
+	}
+
+	get name(): string { return this.identifier.text; }
+	get symbolInformation(): SymbolInformation {
+		return SymbolInformationFactory.create(
+			this, this.symbolKind
+		);
+	}
+}
+
+export class MethodBlockDeclarationElement extends BaseMethodElement {
+	tokenType: SemanticTokenTypes;
+	tokenModifiers: SemanticTokenModifiers[] = [];
+	symbolKind: SymbolKind;
+
+	constructor(context: MethodStmtContext, document: TextDocument) {
+		super(context, document);
+		this.tokenType = SemanticTokenTypes.method;
+		this.symbolKind = SymbolKind.Method;
+	}
+}
+
+abstract class BaseVariableDeclarationStatementElement extends BaseContextSyntaxElement {
+	abstract declarations: VariableDeclarationElement[];
+
+	constructor(context: ConstStmtContext | VariableStmtContext, document: TextDocument) {
+		super(context, document);
+	}
+}
+
+export class ConstDeclarationsElement extends BaseVariableDeclarationStatementElement {
+	declarations: VariableDeclarationElement[] = [];
+
+	constructor(context: ConstStmtContext, document: TextDocument) {
+		super(context, document);
+		context.constSubStmt().forEach((element) =>
+			this.declarations.push(new VariableDeclarationElement(
+				element, document
+			))
+		);
+	}
+}
+
+export class VariableDeclarationsElement extends BaseVariableDeclarationStatementElement {
+	declarations: VariableDeclarationElement[] = [];
+
+	constructor(context: VariableStmtContext, document: TextDocument) {
+		super(context, document);
+		context.variableListStmt().variableSubStmt().forEach((element) =>
+			this.declarations.push(new VariableDeclarationElement(
+				element, document
+			))
+		);
+	}
+}
+
+class VariableDeclarationElement extends BaseContextSyntaxElement implements HasSymbolInformation {
+	identifier: IdentifierElement;
+	asType: VariableType;
+	arrayBounds?: ArrayBounds;
+
+	constructor(context: ConstSubStmtContext | VariableSubStmtContext, document: TextDocument) {
+		super(context, document);
+		this.asType = new VariableType(context.asTypeClause(), document);
+		this.arrayBounds = ArrayBounds.create(context);
+		this.identifier = new IdentifierElement(context.ambiguousIdentifier(), document);
+	}
+
+	get name(): string { return this.identifier.text; }
+	get symbolInformation(): SymbolInformation {
+		return SymbolInformationFactory.create(
+			this, this.asType.symbolKind
+		);
+	}
+}
+
+class VariableType extends BaseSyntaxElement {
+	typeName: string;
+	symbolKind: SymbolKind;
+
+	constructor(context: AsTypeClauseContext | undefined, document: TextDocument, isArray?: boolean) {
+		super(context, document);
+		this.symbolKind = isArray ? SymbolKind.Array : SymbolKind.Variable;
+
+		// Needs more ternery.
+		const type = context?.type_()?.baseType() ?? context?.type_()?.complexType();
+		this.typeName = type?.text ?? type?.text ?? 'Variant';
+		this.symbolKind = type ? type.toSymbolKind() : SymbolKind.Variable;
+	}
+}
+
+class ArrayBounds {
+	dimensions: { lower: number, upper: number }[] = [];
+
+	constructor(subStmt: VariableSubStmtContext) {
+		subStmt.subscripts()?.subscript_().forEach((x) => {
+			const vals = x.valueStmt();
+			this.dimensions.push({
+				lower: vals.length === 1 ? 0 : +vals[0].text,
+				upper: vals.length === 1 ? +vals[0].text : +vals[1].text
+			});
+		});
+	}
+
+	/**
+	 * Creates an ArrayBounds if the context is a variable and an array.
+	 * @param subStmt a subStmt context for a variable or a constant.
+	 * @returns A new array bounds if the context is an array variable.
+	 */
+	static create(subStmt: VariableSubStmtContext | ConstSubStmtContext) {
+		const hasLparenMethod = (x: any): x is VariableSubStmtContext => 'LPAREN' in x;
+		if (hasLparenMethod(subStmt) && subStmt.LPAREN()) {
+			return new ArrayBounds(subStmt);
+		}
+	}
+}
--- server/src/project/workspace.ts
+++ server/src/project/workspace.ts
@@ -1,8 +1,8 @@
-import { CompletionItem, CompletionParams, DidChangeConfigurationNotification, DidChangeConfigurationParams, DidChangeWatchedFilesParams, DocumentSymbolParams, FoldingRange, FoldingRangeParams, Hover, HoverParams, SymbolInformation, WorkspaceFoldersChangeEvent, _Connection } from 'vscode-languageserver';
-import { IProjectDocument } from './document';
+import { CompletionItem, CompletionParams, DidChangeConfigurationNotification, DidChangeConfigurationParams, DidChangeWatchedFilesParams, DocumentSymbolParams, FoldingRange, FoldingRangeParams, Hover, HoverParams, SemanticTokensParams, SemanticTokensRangeParams, SymbolInformation, TextDocuments, WorkspaceFoldersChangeEvent, _Connection } from 'vscode-languageserver';
+import { BaseProjectDocument, ProjectDocument } from './document';
 import { LanguageServerConfiguration } from '../server';
-import { sleep } from '../utils/helpers';
 import { hasConfigurationCapability } from '../capabilities/workspaceFolder';
+import { TextDocument } from 'vscode-languageserver-textdocument';
 
 
 /**
@@ -10,16 +10,25 @@ import { hasConfigurationCapability } from '../capabilities/workspaceFolder';
  * at a workspace level.
  */
 export class Workspace {
-	private events: WorkspaceEvents;
-	private documents: IProjectDocument[] = [];
-	private publicScopeDeclarations: Map<string, any> = new Map();
+	private _events: WorkspaceEvents;
+	private _documents: ProjectDocument[] = [];
+	private _activeDocument?: ProjectDocument;
+	private _publicScopeDeclarations: Map<string, any> = new Map();
 	
 	readonly connection: _Connection;
+
+	activateDocument(document: ProjectDocument) {
+		this._activeDocument = document;
+	}
+
+	get activeDocument() {
+		return this._activeDocument;
+	}
 
 	// constructor(connection: _Connection, capabilities: LanguageServerCapabilities) {
 	constructor(params: {connection: _Connection, capabilities: LanguageServerConfiguration}) {
 		this.connection = params.connection;
-		this.events = new WorkspaceEvents({
+		this._events = new WorkspaceEvents({
 			workspace: this,
 			connection: params.connection,
 			configuration: params.capabilities,
@@ -47,16 +56,23 @@ export class Workspace {
 }
 
 class WorkspaceEvents {
-	readonly workspace: Workspace;
-	readonly configuration: LanguageServerConfiguration;
+	private readonly _workspace: Workspace;
+	private readonly _documents: TextDocuments<TextDocument>;
+	private readonly _configuration: LanguageServerConfiguration;
+
+	activeDocument?: ProjectDocument;
 
 	constructor(params: {connection: _Connection, workspace: Workspace, configuration: LanguageServerConfiguration}) {
-		this.workspace = params.workspace;
-		this.configuration = params.configuration;
+		this._workspace = params.workspace;
+		this._configuration = params.configuration;
+		this._documents = new TextDocuments(TextDocument);
 		this.initialiseConnectionEvents(params.connection);
+		this.initialiseDocumentsEvents();
+		this._documents.listen(params.connection);
 	}
 
 	private initialiseConnectionEvents(connection: _Connection) {
+		console.log('Initialising connection events...');
 		connection.onInitialized(() => this.onInitialized());
 		connection.onCompletion(params => this.onCompletion(params));
 		connection.onCompletionResolve(item => this.onCompletionResolve(item));
@@ -65,50 +81,54 @@ class WorkspaceEvents {
 		connection.onDocumentSymbol((params) => this.onDocumentSymbolAsync(params));
 		connection.onHover(params => this.onHover(params));
 
-		if (hasConfigurationCapability(this.configuration)) {
+		if (hasConfigurationCapability(this._configuration)) {
 			connection.onFoldingRanges((params) => this.onFoldingRanges(params));
 		}
 
 		connection.onRequest((method: string, params: object | object[] | any) => {
 			switch (method) {
 				case 'textDocument/semanticTokens/full': {
-					// const stp = params as SemanticTokensParams;
-					// return docInfo.getSemanticTokens(stp);
-					break;
+					return this.activeDocument?.languageServerSemanticTokens();
 				}
-				case 'textDocument/semanticTokens/range':
-					// return docInfo.getSemanticTokens(params);
-					break;
+				case 'textDocument/semanticTokens/range': {
+					const rangeParams = params as SemanticTokensRangeParams;
+					return this.activeDocument?.languageServerSemanticTokens(rangeParams.range);
+				}
 				default:
-					// console.error(`Unresolved request path: ${method}`);
+					console.error(`Unresolved request path: ${method}`);
 			}
 		});
+	}
+
+	private initialiseDocumentsEvents() {
+		console.log('Initialising documents events...');
+		this._documents.onDidChangeContent(e => this.onDidChangeContent(e.document));
 	}
 
 	/** Connection event handlers */
 
 	private onCompletion(params: CompletionParams): never[] {
-		console.log('onCompletion: ' + params);
+		console.log(`onCompletion: ${params}`);
 		return [];
 	}
 
 	private onCompletionResolve(item: CompletionItem): CompletionItem {
-		console.log('onCompletionResolve: ' + item.label);
+		console.log(`onCompletionResolve: ${item.label}`);
 		return item;
 	}
 
 	private onDidChangeConfiguration(params: DidChangeConfigurationParams): void {
-		console.log('onDidChangeConfiguration: ' + params);
+		console.log(`onDidChangeConfiguration: ${params}`);
 	}
 
 	private onDidChangeWatchedFiles(params: DidChangeWatchedFilesParams) {
-		console.log('onDidChangeWatchedFiles: ' + params);
+		console.log(`onDidChangeWatchedFiles: ${params}`);
 	}
 
 	// TODO: Should trigger a full workspace refresh.
 	private onDidChangeWorkspaceFolders(e: WorkspaceFoldersChangeEvent) {
-		console.log('onDidChangeWorkspaceFolders: ' + e);
-		this.workspace.connection.console.log('Workspace folder change event received.\n' + e);
+		console.log(`onDidChangeWorkspaceFolders: ${e}`);
+		this._workspace.connection.console.log(`Workspace folder change event received.\n${e}`);
 	}
 
 	private async onDocumentSymbolAsync(params: DocumentSymbolParams): Promise<SymbolInformation[]> {
@@ -123,21 +143,35 @@ class WorkspaceEvents {
 	}
 
 	private onHover(params: HoverParams): Hover {
-		console.log('onHover: ' + params.position.line + ',' + params.position.character);
+		console.log(`onHover: ${params.position.line},${params.position.character}`);
 		return { contents: '' };
 	}
 
 	private onInitialized(): void {
 		console.log('onInitialized:---');
-		const connection = this.workspace.connection;
+		const connection = this._workspace.connection;
 		// Register for client configuration notification changes.
 		connection.client.register(DidChangeConfigurationNotification.type, undefined);
 
 		// This is how we can listen for changes to workspace folders.
-		if (hasConfigurationCapability(this.configuration)) {
+		if (hasConfigurationCapability(this._configuration)) {
 			connection.workspace.onDidChangeWorkspaceFolders(e =>
 				this.onDidChangeWorkspaceFolders(e)
 			);
 		}
 	}
+
+	/** Documents event handlers */
+
+	/**
+	 * This event handler is called whenever a `TextDocuments<TextDocument>` is changed.
+	 * @param doc The document that changed.
+	 */
+	onDidChangeContent(doc: TextDocument) {
+		console.log('onDidChangeContent:--- ' + doc.uri);
+		this.activeDocument = BaseProjectDocument.create(this._workspace, doc);
+		this.activeDocument.parse();
+		this._workspace.activateDocument(this.activeDocument);
+	}
+
 }
