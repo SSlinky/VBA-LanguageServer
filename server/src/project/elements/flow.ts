78bb64ace8052ba383cd9c6fdb12c75892e55c15
sslinky <sam.vanderslink@notis.net.au>
2024-11-22 17:32:51 +0800

    add DuplicateOperatorElement class

--- server/src/project/elements/flow.ts
+++ server/src/project/elements/flow.ts
@@ -18,3 +18,16 @@ export class WhileLoopElement extends BaseContextSyntaxElement implements HasDia
 		this.diagnostics.push(new WhileWendDeprecatedDiagnostic(this.range))
 	}
 }
+
+export class DuplicateOperatorElement extends BaseContextSyntaxElement implements HasDiagnosticCapability {
+	diagnostics: Diagnostic[] = [];
+
+	constructor(context: AnyOperatorContext, document: TextDocument) {
+		super(context, document);
+	}
+
+	evaluateDiagnostics() {
+		this.diagnostics.push(new MultipleOperatorsDiagnostic(this.range))
+		return this.diagnostics;
+	}
+}
