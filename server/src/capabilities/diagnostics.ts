d4796918c2f61b8def76fbd2cbfcfeba366515d9
sslinky <sam.vanderslink@notis.net.au>
2024-11-22 17:33:03 +0800

    Refactor and tidy

--- server/src/capabilities/diagnostics.ts
+++ server/src/capabilities/diagnostics.ts
@@ -7,23 +7,26 @@ function hasDiagnosticRelatedInformationCapability(x: TextDocumentClientCapabili
 
 abstract class BaseDiagnostic implements Diagnostic {
 	range: Range;
+	message: string
 	severity?: DiagnosticSeverity | undefined;
 	code?: string | number | undefined;
 	codeDescription?: CodeDescription | undefined;
 	source?: string | undefined;
-	abstract message: string;
 	tags?: DiagnosticTag[] | undefined;
 	relatedInformation?: DiagnosticRelatedInformation[] | undefined;
 	data?: unknown;
 
-	constructor(range: Range) {
+	constructor(range: Range)
+	constructor(range: Range, message: string)
+	constructor(range: Range, message?: string) {
 		this.range = range;
+		this.message = message ?? 'Generic diagnostic.';
 	}
 }
 
 
 export class MultipleOperatorsDiagnostic extends BaseDiagnostic {
-	message = "Unexpected duplicate operator";
+	message = "Unexpected duplicate operator.";
 	constructor(range: Range) {
 		super(range);
 	}
@@ -33,41 +36,36 @@ export class WhileWendDeprecatedDiagnostic extends BaseDiagnostic {
 	message = "The Do...Loop statement provides a more structured and flexible way to perform looping.";
 	severity = DiagnosticSeverity.Information;
 	constructor(range: Range) {
-		super(Range.create(
-			range.start,
-			Position.create(range.start.line, range.start.character + 4)
-		));
+		super(Range.create(range.start, Position.create(range.start.line, range.start.character + 4)));
 	}
 }
 
+// test
 export class MissingAttributeDiagnostic extends BaseDiagnostic {
-	message: string;
 	severity = DiagnosticSeverity.Error;
-
 	constructor(range: Range, attributeName: string) {
-		super(range);
-		this.message = `Module missing attribute ${attributeName}.`;
+		super(range, `Module missing attribute ${attributeName}.`);
 	}
 }
 
+// test
 export class DuplicateAttributeDiagnostic extends BaseDiagnostic {
-	message: string;
 	severity = DiagnosticSeverity.Error;
-
 	constructor(range: Range, attributeName: string) {
-		super(range);
-		this.message = `Module duplicate attribute ${attributeName}.`;
+		super(range, `Module duplicate attribute ${attributeName}.`);
 	}
 }
 
+// test
 export class DuplicateDeclarationDiagnostic extends BaseDiagnostic {
-	message = "Duplicate declaration in current scope";
+	message = "Duplicate declaration in current scope.";
 	severity = DiagnosticSeverity.Error;
 	constructor(range: Range) {
 		super(range);
 	}
 }
 
+// test
 export class ShadowDeclarationDiagnostic extends BaseDiagnostic {
 	message = "Declaration is shadowed in the local scope.";
 	severity = DiagnosticSeverity.Error;
@@ -76,6 +74,7 @@ export class ShadowDeclarationDiagnostic extends BaseDiagnostic {
 	}
 }
 
+// test
 export class IgnoredAttributeDiagnostic extends BaseDiagnostic {
 	message = "This attribute will be ignored.";
 	severity = DiagnosticSeverity.Information;
@@ -92,11 +91,10 @@ export class MissingOptionExplicitDiagnostic extends BaseDiagnostic {
 	}
 }
 
+// test
 export class ElementOutOfPlaceDiagnostic extends BaseDiagnostic {
-	message: string;
 	severity = DiagnosticSeverity.Error;
 	constructor(range: Range, elementName: string) {
-		super(range);
-		this.message = `${elementName}s cannot appear below a Sub, Function, or Property declaration.`
+		super(range, `${elementName}s cannot appear below a Sub, Function, or Property declaration.`);
 	}
 }
