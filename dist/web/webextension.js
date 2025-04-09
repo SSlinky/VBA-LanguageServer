d274087ab4580ec30cf4ef7a3c7031185367c6d2
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2025-04-09 19:46:13 +0200

    enable web extension launch

---
+++ dist/web/webextension.js
@@ -0,0 +1,8 @@
+const vscode = require('vscode')
+
+function activate(context) {
+	// Dummy function for activation in web environment
+	console.log('VBA extension activated')
+}
+
+module.exports = { activate }
