de981449aebb1c4380dd0a6f0c3a4bdc4ece0481
sslinky <39886505+SSlinky@users.noreply.github.com>
2025-04-29 13:29:59 +0800

    Added uri extension methods

--- server/src/extensions/stringExtensions.ts
+++ server/src/extensions/stringExtensions.ts
@@ -1,9 +1,26 @@
-interface String {
-	stripQuotes(): string;
-}
+import { fileURLToPath, pathToFileURL } from "url";
 
+declare global {
+	interface String {
+		stripQuotes(): string;
+		toFilePath(): string;
+		toFileUri(): string;
+	}
+}
 
 String.prototype.stripQuotes = function (): string {
 	const exp = /^"?(.*?)"?$/;
 	return exp.exec(this.toString())![1];
+};
+
+String.prototype.toFilePath = function (): string {
+	return this.startsWith('file://')
+		? fileURLToPath(this.toString())
+		: this.toString();
+};
+
+String.prototype.toFileUri = function (): string {
+	return this.startsWith('file://')
+		? pathToFileURL(this.toString()).href
+		: this.toString();
 };
