1348c8ad61ffc66c6f4d387888c57525eb1c3f02
sslinky <39886505+SSlinky@users.noreply.github.com>
2025-05-17 21:36:15 +0800

    Added new context extensions

--- server/src/extensions/antlrVbaParserExtensions.ts
+++ server/src/extensions/antlrVbaParserExtensions.ts
@@ -4,23 +4,31 @@ import { SymbolKind } from 'vscode-languageserver';
 // Project
 import {
     AmbiguousIdentifierContext,
-    BuiltinTypeContext,
-    ClassTypeNameContext,
+    ArrayClauseContext,
+    ArrayDesignatorContext,
+    ArrayDimContext,
+    AsClauseContext,
     ConstItemContext,
-    TypeSpecContext,
+    LowerBoundContext,
+    PositionalParamContext,
+    TypeExpressionContext,
     TypeSuffixContext,
+    UpperBoundContext,
     VariableDclContext,
     WitheventsVariableDclContext
 } from '../antlr/out/vbaParser';
+import { ParserRuleContext } from 'antlr4ng';
 
+export type ArrayDimension = {
+    lowerBound: number;
+    upperBound: number;
+}
 
 declare module '../antlr/out/vbaParser' {
 
     interface ConstItemContext {
-        /** Shortcut the identifier as we know it will always exist. */
-        ambiguousIdentifier(): AmbiguousIdentifierContext;
         /** Shortcut to get the type context */
-        typeContext(): BuiltinTypeContext | TypeSuffixContext | undefined;
+        typeContext(): ParserRuleContext | undefined;
         /** Extension method to get the symbol kind. */
         toSymbolKind(): SymbolKind;
     }
@@ -29,7 +37,7 @@ declare module '../antlr/out/vbaParser' {
         /** Shortcut the identifier as we know it will always exist. */
         ambiguousIdentifier(): AmbiguousIdentifierContext;
         /** Shortcut to get the type context */
-        typeContext(): TypeSpecContext | TypeSuffixContext | ClassTypeNameContext | undefined;
+        typeContext(): ParserRuleContext | undefined;
         /** Extension method to get the symbol kind. */
         toSymbolKind(): SymbolKind;
     }
@@ -38,11 +46,53 @@ declare module '../antlr/out/vbaParser' {
         /** Shortcut the identifier as we know it will always exist. */
         ambiguousIdentifier(): AmbiguousIdentifierContext;
         /** Shortcut to get the type context */
-        typeContext(): TypeSpecContext | TypeSuffixContext | ClassTypeNameContext | undefined;
+        typeContext(): ParserRuleContext | undefined;
         /** Extension method to get the symbol kind. */
         toSymbolKind(): SymbolKind;
     }
+
+    interface PositionalParamContext {
+        /** Shortcut the identifier as we know it will always exist. */
+        ambiguousIdentifier(): AmbiguousIdentifierContext;
+        /** Shortcut to get the type context */
+        typeContext(): ParserRuleContext | undefined;
+        /** Extension method to get the symbol kind. */
+        toSymbolKind(): SymbolKind;
+    }
+
+    interface AsClauseContext {
+        isObject: boolean;
+        isVariant: boolean;
+        isPrimative: boolean;
+    }
+
+    interface TypeExpressionContext {
+        isObject: boolean;
+        isVariant: boolean;
+        isPrimative: boolean;
+    }
+
+    interface TypeSuffixContext {
+        isObject: boolean;
+        isVariant: boolean;
+        isPrimative: boolean;
+    }
+
+    interface ArrayDesignatorContext {
+        isResizable: boolean;
+        getDimensions(): ArrayDimension[] | undefined;
+    }
+
+    interface ArrayDimContext {
+        isResizable: boolean;
+        getDimensions(): ArrayDimension[] | undefined;
+    }
 }
+
+ConstItemContext.prototype.typeContext = function (): ParserRuleContext | undefined {
+    return this.typeSuffix()
+        ?? this.constAsClause()?.builtinType();
+};
 
 VariableDclContext.prototype.ambiguousIdentifier = function (): AmbiguousIdentifierContext {
     // A variable will always be typed or untyped.
@@ -51,10 +101,114 @@ VariableDclContext.prototype.ambiguousIdentifier = function (): AmbiguousIdentif
 };
 
 
-VariableDclContext.prototype.typeContext = function (): TypeSpecContext | TypeSuffixContext | ClassTypeNameContext | undefined {
+VariableDclContext.prototype.typeContext = function (): ParserRuleContext | undefined {
     return this.typedVariableDcl()?.typedName().typeSuffix()
         ?? this.untypedVariableDcl()?.asClause()?.asType()?.typeSpec()
         ?? this.untypedVariableDcl()?.asClause()?.asAutoObject()?.classTypeName();
+};
+
+PositionalParamContext.prototype.ambiguousIdentifier = function (): AmbiguousIdentifierContext {
+    return this.paramDcl().untypedNameParamDcl()?.ambiguousIdentifier()
+        ?? this.paramDcl().typedNameParamDcl()!.typedName().ambiguousIdentifier()!;
+};
+
+PositionalParamContext.prototype.typeContext = function (): ParserRuleContext | undefined {
+    return this.paramDcl().untypedNameParamDcl()?.parameterType()?.typeExpression()
+        ?? this.paramDcl().typedNameParamDcl()?.typedName().typeSuffix();
+};
+
+
+// Type Properties
+
+Object.defineProperty(TypeExpressionContext.prototype, "isPrimative", {
+    get: function (this: TypeExpressionContext): boolean {
+        return !!this.builtinType() && !this.isVariant;
+    }
+});
+
+Object.defineProperty(TypeExpressionContext.prototype, "isVariant", {
+    get: function (this: TypeExpressionContext): boolean {
+        return !!this.builtinType()?.reservedTypeIdentifier()?.VARIANT()
+            || !!this.builtinType()?.reservedTypeIdentifierB()?.VARIANT_B();
+    }
+});
+
+Object.defineProperty(TypeExpressionContext.prototype, "isObject", {
+    get: function (this: TypeExpressionContext): boolean {
+        return !this.isPrimative && !this.isVariant;
+    }
+});
+
+Object.defineProperty(TypeSuffixContext.prototype, "isPrimative", {
+    get: function (this: TypeSuffixContext): boolean {
+        return true;
+    }
+});
+
+Object.defineProperty(TypeSuffixContext.prototype, "isVariant", {
+    get: function (this: TypeSuffixContext): boolean {
+        return false;
+    }
+});
+
+Object.defineProperty(TypeSuffixContext.prototype, "isObject", {
+    get: function (this: TypeSuffixContext): boolean {
+        return false;
+    }
+});
+
+Object.defineProperty(AsClauseContext.prototype, "isPrimative", {
+    get: function (this: AsClauseContext): boolean {
+        const typeSpecContext = this.asType()?.typeSpec();
+        return !!(typeSpecContext
+            && (typeSpecContext.fixedLengthStringSpec() || typeSpecContext.typeExpression()?.isPrimative)
+        );
+    }
+});
+
+Object.defineProperty(AsClauseContext.prototype, "isVariant", {
+    get: function (this: AsClauseContext): boolean {
+        return this.asType()?.typeSpec().typeExpression()?.isVariant ?? false;
+    }
+});
+
+Object.defineProperty(AsClauseContext.prototype, "isObject", {
+    get: function (this: AsClauseContext): boolean {
+        const isAutoObject = !!this.asAutoObject();
+        const isTypeExprObject = !!(this.asType()?.typeSpec().typeExpression() ?? false);
+        return isAutoObject || isTypeExprObject;
+    }
+});
+
+Object.defineProperty(ArrayDesignatorContext.prototype, "isResizable", {
+    get: function (this: ArrayDesignatorContext): boolean {
+        return false;
+    }
+});
+
+ArrayDesignatorContext.prototype.getDimensions = function (): ArrayDimension[] | undefined {
+    return undefined;
+};
+
+Object.defineProperty(ArrayClauseContext.prototype, "isResizable", {
+    get: function (this: ArrayClauseContext): boolean {
+        return !!this.arrayDim().boundsList();
+    }
+});
+
+ArrayDimContext.prototype.getDimensions = function (): ArrayDimension[] | undefined {
+    const boundsListCtx = this.boundsList();
+    const getNum = (boundCtx: LowerBoundContext | UpperBoundContext | null): number => {
+        const FIXME_DEFAULT = 0;
+        const n = boundCtx?.constantExpression().expression().literalExpression()?.INTEGERLITERAL()?.getText();
+        return n === undefined ? FIXME_DEFAULT : Number.parseInt(n);
+    };
+    return !boundsListCtx ? undefined : boundsListCtx.dimSpec().map(dimensionCtx => {
+        return {
+            lowerBound: getNum(dimensionCtx.lowerBound()),
+            upperBound: getNum(dimensionCtx.upperBound())
+        };
+    });
 };
 
 
@@ -74,8 +228,12 @@ VariableDclContext.prototype.toSymbolKind = function (): SymbolKind {
     return toSymbolKind(this.typeContext());
 };
 
+PositionalParamContext.prototype.toSymbolKind = function (): SymbolKind {
+    return toSymbolKind(this.typeContext());
+};
 
-function toSymbolKind(context: BuiltinTypeContext | TypeSuffixContext | TypeSpecContext | ClassTypeNameContext | undefined): SymbolKind {
+
+function toSymbolKind(context: ParserRuleContext | undefined): SymbolKind {
     switch (context?.getText().toLocaleLowerCase()) {
         case undefined:
             return SymbolKind.Class;
